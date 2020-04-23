f5927b3602771bd0c9e6fab04d04e37257d0c310
JM-Mostafa-Kamal <jm.mostafakamal@gmail.com>
2020-04-23 16:16:55 +0600

    My assignment work!

---
+++ cachematrix.R
@@ -0,0 +1,44 @@
+# what this functions do: 
+## Write a short comment describing this function
+
+# the function work flows -
+# set the value of the matrix;
+# get the value of the matrix;
+# set the value of the inverse;
+# get the value of the inverse;
+
+
+makeCacheMatrix <- function(x = matrix()) {
+  i <- NULL
+  set <- function(y) {
+    x <<- y
+    i <<- NULL
+  }
+  get <- function() x
+  setinverse <- function(inverse) i <<- inverse
+  getinverse <- function() i
+  list(set = set,
+       get = get,
+       setinverse = setinverse,
+       getinverse = getinverse)
+}
+
+
+## Write a short comment describing this function
+
+# it solves the matrix 
+
+
+cacheSolve <- function(x, ...) {
+  i <- x$getinverse()
+  if (!is.null(i)) {
+    message("getting cached data")
+    return(i)
+  }
+  data <- x$get()
+  i <- solve(data, ...)
+  x$setinverse(i)
+  i
+}
+
+
